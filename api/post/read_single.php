1ee6d758a253a573fe6394b7ffa82984b9c50c51
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-06-03 14:41:38 +0300

    feat: API database read one

--- api/post/read_single.php
+++ api/post/read_single.php
@@ -15,15 +15,18 @@
     $result = $post->read_single();
 
     $row = $result->fetch(PDO::FETCH_ASSOC);
-
-    $post_arr = array(
-        'id' => $row['id'],
-        'title' => $row['title'],
-        'body' => html_entity_decode($row['body']),
-        'author' => $row['author'],
-        'category_id' => $row['category_id'],
-        'category_name' => $row['category_name'],
-        'created_at' => $row['created_at']
-    );
-
-    print_r(json_encode($post_arr));
+    if ($row) {
+        $post_arr = array(
+            'id' => $row['id'],
+            'title' => $row['title'],
+            'body' => html_entity_decode($row['body']),
+            'author' => $row['author'],
+            'category_id' => $row['category_id'],
+            'category_name' => $row['category_name'],
+            'created_at' => $row['created_at']
+        );
+    
+        print_r(json_encode($post_arr));
+    } else {
+        print_r(json_encode(array('message' => 'No posts found')));
+    }
