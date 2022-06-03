152b18445b55370ea44f01e98159315cc45a0132
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-06-03 14:01:01 +0300

    feat: API database read

--- config/Database.php
+++ config/Database.php
@@ -13,7 +13,7 @@ class Database {
             $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $exc) {
-            echo "Database connection Error: $e->getMessage()";
+            echo "Database connection Error: " . $exc->getMessage();
         }
 
         return $this->conn;
