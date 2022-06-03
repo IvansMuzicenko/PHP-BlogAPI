169cb6f833327b725bf748703a89675756c69782
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-06-03 14:37:47 +0300

    feat: API database read one

---
+++ api/post/read_single.php
@@ -0,0 +1,29 @@
+<?php 
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Post.php';
+
+    $database = new Database();
+    $db = $database->connect();
+
+    $post = new Post($db);
+
+    $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+    $result = $post->read_single();
+
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+
+    $post_arr = array(
+        'id' => $row['id'],
+        'title' => $row['title'],
+        'body' => html_entity_decode($row['body']),
+        'author' => $row['author'],
+        'category_id' => $row['category_id'],
+        'category_name' => $row['category_name'],
+        'created_at' => $row['created_at']
+    );
+
+    print_r(json_encode($post_arr));
